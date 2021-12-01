66d0569c55bfdae274ef7af10801531303988812
Razvan <91622564+915-Petruta-Razvan@users.noreply.github.com>
2021-12-01 21:41:39 +0200

    Updates

---
+++ precache-manifest.8ed1561c87fad14f6a7764bc637f4e5a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da696f7f6d03b22c62085b6f59a53681",
+    "url": "/idea_app_react/index.html"
+  },
+  {
+    "revision": "de7d52ae9d7be7316816",
+    "url": "/idea_app_react/static/js/2.4fc9b6bb.chunk.js"
+  },
+  {
+    "revision": "ebae5c71935e26733006",
+    "url": "/idea_app_react/static/js/main.67350374.chunk.js"
+  },
+  {
+    "revision": "f471b94d5f3bff336b81",
+    "url": "/idea_app_react/static/js/runtime-main.622748ac.js"
+  }
+]);
